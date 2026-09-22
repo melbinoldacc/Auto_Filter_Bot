6d10aaed1db71f3925011f08c83df82a558a8bf2
melbinoldacc <153085430+melbinoldacc@users.noreply.github.com>
2026-09-22 11:49:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -118,7 +118,7 @@ async def start(client, message):
                         InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                     ],[
                         InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                        InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
+                        InlineKeyboardButton(' ᴊᴏɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ 👥', url='https://t.me/cinephilegrp')
                     ],]
             reply_markup = InlineKeyboardMarkup(buttons)
             current_time = datetime.now(pytz.timezone(TIMEZONE))
@@ -148,7 +148,7 @@ async def start(client, message):
                         InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                     ],[
                         InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                        InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
+                        InlineKeyboardButton(' ᴊᴏɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ 👥', url='https://t.me/cinephilegrp')
                     ],]
             reply_markup = InlineKeyboardMarkup(buttons)
             current_time = datetime.now(pytz.timezone(TIMEZONE))
